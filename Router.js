b1aea2454b1d04948c1518dde9ca35e3a31a6cd6
Algirdas <mrinter5@gmail.com>
2017-04-09 19:42:54 +0000

    Initial Progress: part F

---
+++ Router.js
@@ -0,0 +1,57 @@
+import net from 'net';
+import EventEmitter from 'events';
+import debug from 'debug';
+
+const log = debug('fwdizer:Router');
+
+export default class Router extends EventEmitter {
+    constructor({
+        port
+    }) {
+        const socketQueue = [];
+
+
+
+        const server = this.server = net.createServer();
+
+        server.listen(port);
+
+        // Bail on any server error.
+        server.on('error', (error) => {
+            log(`Server Error: ${error.toString()}`);
+            this.selfDestruct();
+        });
+
+        server.on('close', () => {
+            log('Server Closed');
+            this.selfDestruct();
+        });
+
+        server.on('connection', (socket) => {
+            log('New Client on Server');
+
+            socketQueue.push(socket);
+
+            this.emit('newConnection');
+        });
+
+
+
+        this.on('newTunnel', (tunnelSocket) => {
+            if (socketQueue.length === 0) {
+                tunnelSocket.end();
+            } else {
+                const queuedSocket = socketQueue.shift();
+
+                tunnelSocket.pipe(queuedSocket);
+                queuedSocket.pipe(tunnelSocket);
+            }
+        });
+    }
+
+    selfDestruct() {
+        log('Self-Destructing');
+
+        this.emit('selfDestruct');
+    }
+}
--- RouterServer.js
+++
@@ -1,9 +0,0 @@
-import net from 'net';
-import EventEmitter from 'events';
-import debug from 'debug';
-
-const log = debug('RouterServer');
-
-export default class RouterServer extends EventEmitter {
-
-}
